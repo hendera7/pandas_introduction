c81380aa284f761c2bff6c0cd65d033c3cfd63ae
hendera2 <hendera2@oregonstate.edu>
2022-08-22 22:09:01 -0500

    formulas added

--- linear_regression_1.py
+++ linear_regression_1.py
@@ -11,10 +11,23 @@ FILE_PATH = 'all_perth_310121.csv'
 raw = pd.read_csv(FILE_PATH)
 n_raw = raw.dropna()  # data set which drops rows with n/a values
 
-print(n_raw.corr())  # shows the correlation between all the data columns
 
-# n_raw.plot(kind = 'scatter', x = 'FLOOR_AREA', y = 'PRICE')
-# n_raw.plot(kind = 'scatter', x = 'BEDROOMS', y = 'PRICE')
-n_raw.plot(kind = 'scatter', x = 'BATHROOMS', y = 'PRICE')
+def correlation(data_file):
+    print(data_file.corr())  # shows the correlation between all the data columns
+    return
 
-plt.show()
+def price_floor_area(data_file):
+    data_file.plot(kind = 'scatter', x = 'FLOOR_AREA', y = 'PRICE')
+    return plt.show()
+
+def price_bedrooms(data_file):
+    data_file(kind = 'scatter', x = 'BEDROOMS', y = 'PRICE')
+    return plt.show()
+
+def price_bathrooms(data_file):
+    data_file(kind = 'scatter', x = 'BATHROOMS', y = 'PRICE')
+    return plt.show()
+
+
+test = price_floor_area(n_raw)
+print(test)
