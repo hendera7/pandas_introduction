d9cdcf5ab15da714d5c92005681f9656716610a9
hendera7 <106762940+hendera7@users.noreply.github.com>
2022-08-25 16:33:08 -0500

    updates 8/25

--- linear_regression_1.py
+++ linear_regression_1.py
@@ -1,6 +1,7 @@
 import math
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 # from sklearn.linear_model import LinearRegression
@@ -26,10 +27,34 @@ def price_bedrooms(data_file):
     return plt.show()
 
 def price_bathrooms(data_file):
-    data_file(kind = 'scatter', x = 'BATHROOMS', y = 'PRICE')
+    # data_file(kind = 'scatter', x = 'BATHROOMS', y = 'PRICE')
+    fig = data_file.plot(kind = 'scatter', x = 'BATHROOMS', y = 'PRICE')
+    return fig
+    # return plt.show()
+
+def price_build_year(data_file):
+    # data_file(kind = 'scatter', x = 'BUILD_YEAR', y = 'PRICE')
+    fig = data_file.plot(kind = 'scatter', x = 'BUILD_YEAR', y = 'PRICE')
+    return fig
+    # return plt.show()
+
+def price_build_year_1 (data_file):
+    data_file.plot(kind = 'scatter', x = 'BUILD_YEAR', y = 'PRICE')
+    # fig = data_file.plot(kind = 'scatter', x = 'BUILD_YEAR', y = 'PRICE')
     return plt.show()
+
+
+def plot(data_frame_1, data_frame_2):
+    figure, axes = plt.subplots(nrows = 2, ncols = 2)
+    data_frame_1.plot(ax=axes[0,0])
+    data_frame_2.plot(ax=axes[0,1])
+    plt.show()
 
 
 correlate = correlation(raw_dropped)
 print(correlate)
-print(raw_dropped)
+# print(raw_dropped)
+
+price_build_year_1(raw_dropped)
+
+# plot(price_bathrooms(raw_dropped), price_build_year(raw_dropped))
